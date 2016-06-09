3a4f6d1c0b72cfe2ef3cd769d096420c7576aef1
Peter Bakhirev <peter@pcb.ng>
2016-06-09 09:54:14 -0400

    trying to work around the PythonDialogBug
    following advice from https://github.com/certbot/certbot/issues/1154

--- request-cloudfront.sh
+++ request-cloudfront.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-letsencrypt --renew-by-default --config-dir /etc/letsencrypt --email ${LETSENCRYPT_EMAIL_ADDRESS} --agree-tos -a letsencrypt-s3front:auth \
+letsencrypt --text --non-interactive --renew-by-default --config-dir /etc/letsencrypt --email ${LETSENCRYPT_EMAIL_ADDRESS} --agree-tos -a letsencrypt-s3front:auth \
 --letsencrypt-s3front:auth-s3-bucket $2 \
 -i letsencrypt-s3front:installer \
 --letsencrypt-s3front:installer-cf-distribution-id $3 \
