9ba60fc524b4d40bf85fd045296f0746bc7fe581
Peter Bakhirev <peter@pcb.ng>
2017-08-04 16:45:48 -0400

    try to make cloudfront work with wrong ssl cert

--- request-cloudfront.sh
+++ request-cloudfront.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-letsencrypt --text --non-interactive --renew-by-default --config-dir /etc/letsencrypt --email ${LETSENCRYPT_EMAIL_ADDRESS} --agree-tos -a letsencrypt-s3front:auth \
+letsencrypt --text --non-interactive --renew-by-default --allow-subset-of-names --preferred-challenges http --renew-with-new-domains --config-dir /etc/letsencrypt --email ${LETSENCRYPT_EMAIL_ADDRESS} --agree-tos -a letsencrypt-s3front:auth \
   --letsencrypt-s3front:auth-s3-bucket $3 \
   -i letsencrypt-s3front:installer \
   --letsencrypt-s3front:installer-cf-distribution-id $4 \
